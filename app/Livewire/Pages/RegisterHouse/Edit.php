439016fb28f7137ed60461479e16a269d378d28c
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-14 19:44:55 -0300

    create livewire components update houses

---
+++ app/Livewire/Pages/RegisterHouse/Edit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Livewire\Pages\RegisterHouse;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.register-house.edit')
+            ->layout('layouts.app');
+    }
+}
