692a26ff51b4659d4746bde7b335277c1fa10aa8
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-17 09:32:37 -0300

    create testing create a houses

---
+++ tests/Feature/Livewire/DatabaseHouseTest.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Livewire\Pages\RegisterHouse\Create;
+use Livewire\Livewire;
+use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseHas;
+use function Pest\Livewire\livewire;
+
+it('should be able o create a houses', function () {
+
+    Livewire(Create::class)
+        ->set('title', 'House 1')
+        ->set('city', 'Sao paulo')
+        ->set('price', 199)
+        ->set('email', 'house@housegmai.com')
+        ->set('description', 'Criando house 1')
+        ->call('save')
+        ->assertHasNoErrors();
+
+    assertDatabaseHas('houses', [
+        'title' => 'House 1',
+        'city' => 'Sao paulo',
+        'price' => 199,
+        'email' => 'house@housegmai.com',
+        'description' => 'Criando house 1',
+    ]);
+
+    assertDatabaseCount('houses', 1);
+});
