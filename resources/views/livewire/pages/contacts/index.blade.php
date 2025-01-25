a000236ab05552747b1d2944b3a20e8d82e3653f
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-01-25 15:52:04 -0300

    create form contact and back end

--- resources/views/livewire/pages/contacts/index.blade.php
+++ resources/views/livewire/pages/contacts/index.blade.php
@@ -31,13 +31,13 @@
         <section class="w-full flex-1 flex justify-center items-center flex-col gap-4 p-4 pl-72 mt-3">
             <h1 class="text-2xl font-semibold text-primary">Formulário para contato</h1>
             <div class="bg-white w-96 h-96 drop-shadow-2xl">
-                <form>
+                <form wire:submit.prevent="save">
                     <div class="p-6">
                         <label>
                             Seu email
                         </label>
                         <div class="mt-2">
-                            <input type="text" name="email" id="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-secondary sm:text-sm/6">
+                            <input type="email" wire:model="email" name="email" id="email" class="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-secondary sm:text-sm/6">
                         </div>
                     </div>
 
@@ -52,7 +52,7 @@
 
                     <div class="pl-9">
                         <button
-                            type="button"
+                            type="submit"
                             class="bg-primary text-white text-center px-20 py-3 flex">
                             Enviar mensagem
                         </button>
